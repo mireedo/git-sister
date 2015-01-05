0d4a3ef9db56de28b106cf97cd9a1117ac033828
kundaime <kundaime@fas-PC>
2015-01-05 21:46:52 +0700

    thread class

---
+++ TimerBattleSystem/src/timerbattlesystem/ThreadDemo.java
@@ -0,0 +1,37 @@
+package timerbattlesystem;
+
+public class ThreadDemo extends Thread {
+   private Thread t;
+   private String threadName;
+   
+   ThreadDemo( String name){
+       threadName = name;
+       System.out.println("Creating " +  threadName );
+   }
+   public void run() {
+		Actor hero_1 = new Actor("hero_1");
+                
+      System.out.println("Running " +  threadName );
+      try {
+         for(int i = 4; i > 0; i--) {
+            System.out.println("Thread: " + threadName + ", " + i);
+            // Let the thread sleep for a while.
+            Thread.sleep(50);
+         }
+     } catch (InterruptedException e) {
+         System.out.println("Thread " +  threadName + " interrupted.");
+     }
+     System.out.println("Thread " +  threadName + " exiting.");
+   }
+   
+   public void start ()
+   {
+      System.out.println("Summon " +  threadName );
+      if (t == null)
+      {
+         t = new Thread (this, threadName);
+         t.start ();
+      }
+   }
+
+}
